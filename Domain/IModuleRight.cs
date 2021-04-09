38a024dc364b4d92aac25dedb65486c0cb5adc3d
SirPatagon <sirpatagon@gmail.com>
2021-04-09 22:20:37 +0200

    Initial commit
    Outsorced authorization into a plugin project and created some interfaces to remember how I planned the logic.

---
+++ Domain/IModuleRight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Puzzle.Core.Interfaces.Data
+{
+    public interface IModuleRight
+    {
+        /// <summary>
+        /// <see cref="Guid"/> of a specific module.
+        /// </summary>
+        public Guid ModuleID { get; }
+
+        /// <summary>
+        /// If creation is allowed.
+        /// </summary>
+        public bool Create { get; }
+
+        /// <summary>
+        /// If insertion is allowed.
+        /// </summary>
+        public bool Insert { get; }
+
+        /// <summary>
+        /// If updating is allowed.
+        /// </summary>
+        public bool Update { get; }
+
+        /// <summary>
+        /// If deletion is allowed.
+        /// </summary>
+        public bool Delete { get; }
+    }
+}
---
+++ Domain/IUserModuleParentRights.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Puzzle.Core.Interfaces.Data
+{
+    /// <summary>
+    /// Which rights the user should have for a specific module.
+    /// </summary>
+    public interface IUserModuleParentRights
+    {
+        /// <summary>
+        /// Foreign key to <see cref="IUser"/>.
+        /// </summary>
+        public Guid UserID { get; }
+
+        /// <summary>
+        /// <see cref="Guid"/> of a specific module.
+        /// </summary>
+        public Guid ModuleID { get; }
+    }
+}
